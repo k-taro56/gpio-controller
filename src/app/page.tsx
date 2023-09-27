36fb91f49fc752abba899255990429545599aa14
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-09-27 10:48:28 +0900

    remove unnecessary div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,47 +25,45 @@ export default function Home() {
   }
 
   return (
-    <div className="dark:bg-gray-800">
-      <div className="flex justify-center items-center h-screen">
-        <div className="flex flex-col items-center space-y-4 dark:text-white h-full w-96 p-8">
-          <div className="flex h-full w-full">
-            <Slider
-              value={leftValue}
-              onChange={(val) => {
-                setLeftValue(val);
-                sendData('left', val);
-              }}
-            />
-            <Slider
-              value={rightValue}
-              onChange={(val) => {
-                setRightValue(val);
-                sendData('right', val);
-              }}
-            />
-          </div>
-          <div className="flex w-full">
-            <div className="flex w-1/2 justify-center">
-              <div className="mr-1">Left:</div>
-              <div>{leftValue.toFixed(1)}</div>
-            </div>
-            <div className="flex w-1/2 justify-center">
-              <div className="mr-1">Right:</div>
-              <div>{rightValue.toFixed(1)}</div>
-            </div>
-          </div>
-          <button
-            className="w-full bg-blue-400 rounded-lg"
-            onClick={() => {
-              setLeftValue(0.0);
-              setRightValue(0.0);
-              sendData('left', 0.0);
-              sendData('right', 0.0);
+    <div className="flex justify-center items-center h-screen dark:bg-gray-800">
+      <div className="flex flex-col items-center space-y-4 dark:text-white h-full w-96 p-8">
+        <div className="flex h-full w-full">
+          <Slider
+            value={leftValue}
+            onChange={(val) => {
+              setLeftValue(val);
+              sendData('left', val);
             }}
-          >
-            Stop
-          </button>
+          />
+          <Slider
+            value={rightValue}
+            onChange={(val) => {
+              setRightValue(val);
+              sendData('right', val);
+            }}
+          />
         </div>
+        <div className="flex w-full">
+          <div className="flex w-1/2 justify-center">
+            <div className="mr-1">Left:</div>
+            <div>{leftValue.toFixed(1)}</div>
+          </div>
+          <div className="flex w-1/2 justify-center">
+            <div className="mr-1">Right:</div>
+            <div>{rightValue.toFixed(1)}</div>
+          </div>
+        </div>
+        <button
+          className="w-full bg-blue-400 rounded-lg"
+          onClick={() => {
+            setLeftValue(0.0);
+            setRightValue(0.0);
+            sendData('left', 0.0);
+            sendData('right', 0.0);
+          }}
+        >
+          Stop
+        </button>
       </div>
     </div>
   );
