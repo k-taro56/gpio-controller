02d994f5ada2468ddc032591d63d8d75de734bc1
k-taro56 <k-taro56@users.noreply.github.com>
2024-06-07 07:19:23 +0000

    deploy: b6f47d2f00b6a20e438713369c304a2884f77393

--- _next/static/BojR8HGqXaemCyuFv0Vd5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-47989b205f3bab7d.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
